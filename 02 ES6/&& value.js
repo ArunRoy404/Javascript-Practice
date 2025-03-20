cf06b8b2cec3fdc205388dff827cccf2afe88f77
ArunRoy404 <arunroy0065@gmail.com>
2025-03-20 22:05:52 +0600

    git

---
+++ 02 ES6/&& value.js
@@ -0,0 +1,10 @@
+
+const isTrue = true
+// const isTrue = ''
+// const isTrue = 0
+const name = "x"
+
+// if first value is truthy then newName = second value 
+const newName = isTrue && name
+
+console.log(newName)
---
+++ 02 ES6/or value.js
@@ -0,0 +1,8 @@
+// const isTrue = 'y'
+const isTrue = ''
+const default_name = 'x'
+
+
+// if first value is truthy then newName = first value 
+const name = isTrue || default_name
+console.log(name)
