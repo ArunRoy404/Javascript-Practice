7127a4d12b91c9d8535c1fd3d2a584ebc1135b78
ArunRoy404 <arunroy0065@gmail.com>
2025-03-25 19:01:07 +0600

    useState

---
+++ 02 ES6/useState.js
@@ -0,0 +1,15 @@
+// const useState = (initialState) =>{
+//     let state = [initialState]
+//     const setState = (s) =>{
+//         state[0] = s
+//     }
+//     return [state, setState]
+// }
+
+// const [state, setState] = useState(0)
+
+// console.log(state[0])
+// setState(10)
+// console.log(state[0])
+// setState(121)
+// console.log(state[0])
